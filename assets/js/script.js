41108f02f79b70f0d3ecf725fd18fed31571183a
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-26 14:16:23 -0400

    assign button, user input, and query url to variables in js, and create a event listener for the search button while console logging to check functionaliy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 const weatherAPIKey = "d4ed3cef19cf11d3ba421c419e0afec8";
+let userCity = $("#enter-city");
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=" + weatherAPIKey;
+const searchButton = $(".btn");
+
+searchButton.on("click", function(){
+    console.log("hello");
+});
