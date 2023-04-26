5617db6e8a4f59efb0359fe482acdb46ae8873bb
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-26 19:23:23 -0400

    add code in the click event to pull data from API and place on the page, start another function to get five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 const weatherAPIKey = "d4ed3cef19cf11d3ba421c419e0afec8";
-let userCity = $("#enter-city");
-let citySearched, cityToFind, citySelected;
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=" + weatherAPIKey + "&units=imperial";
+// let citySearched, cityToFind, citySelected;
 const searchButton = $(".btn");
 let searchHistory = $("#search-history");
 let currentCity = $(".current-city");
@@ -10,77 +8,106 @@ let currentTemp = $(".current-temp");
 let currentWind = $(".current-wind");
 let currentHumidity = $(".current-humidity");
 
-let buttonCount = 0;
-let buttonList = [];
-let searchHistoryList = [];
-let newSearch = false;
-let buttonClicked = false;
-let refreshed = false;
+// let buttonCount = 0;
+// let buttonList = [];
+// let searchHistoryList = [];
+// let newSearch = false;
+// let buttonClicked = false;
+// let refreshed = false;
 
-function getCurrentWeatherData(weather) {
-    if (newSearch === true) {
-        citySearched = userCity.value;
-        cityToFind = userCity.value;
-        currentCity.text(citySearched);
-    }
-    else {
-        if (buttonClicked){
-            cityToFind = citySelected;
-            buttonClicked = false;
-        }
-        else {
-            if (refreshed = true) {
-                currentCity.text("Philadelphia");
-                cityToFind = "Philadelphia";
-            }
-            else {
-                cityToFind = userCity.value;
-                currentCity.text(cityToFind);
-            }
-        }
-    }
+// function getCurrentWeatherData(weather) {
+//     if (newSearch === true) {
+//         citySearched = userCity.value;
+//         cityToFind = userCity.value;
+//         currentCity.text(citySearched);
+//     }
+//     else {
+//         if (buttonClicked){
+//             cityToFind = citySelected;
+//             buttonClicked = false;
+//         }
+//         else {
+//             if (refreshed = true) {
+//                 currentCity.text("Philadelphia");
+//                 cityToFind = "Philadelphia";
+//             }
+//             else {
+//                 cityToFind = userCity.value;
+//                 currentCity.text(cityToFind);
+//             }
+//         }
+//     }
     
-}
+// }
 
-function userDataProcessing() {
-    if (userCity.value !== ""){
-        if (searchHistoryList !== null) {
-            if (searchHistoryList.includes(userCity.value) === false){
-                if (buttonCount > 7){
-                    searchHistoryList.shift();
-                    searchHistoryList.push(userCity.value);
-                    $("buttonList[0]").removeAttr("id");
-                    buttonList.shift();
-                }
-                else {
-                    searchHistoryList.push(userCity.value);
-                    buttonCount++;
-                }
-                newSearch = true;
-            }
-            else {
-                newSearch = false;
-            }
-        }
-        else {
-            searchHistoryList = [];
-            searchHistoryList.push(userCity.value);
-            newSearch = true;
-            buttonCount++;
-        }
-    }
-    else {
-        newSearch = false;
-    }
-}
+// function userDataProcessing() {
+//     if (userCity.val() !== ""){
+//         if (searchHistoryList !== null) {
+//             if (searchHistoryList.includes(userCity.value) === false){
+//                 if (buttonCount > 7){
+//                     searchHistoryList.shift();
+//                     searchHistoryList.push(userCity.value);
+//                     $("buttonList[0]").removeAttr("id");
+//                     buttonList.shift();
+//                 }
+//                 else {
+//                     searchHistoryList.push(userCity.value);
+//                     buttonCount++;
+//                 }
+//                 newSearch = true;
+//             }
+//             else {
+//                 newSearch = false;
+//             }
+//         }
+//         else {
+//             searchHistoryList = [];
+//             searchHistoryList.push(userCity.value);
+//             newSearch = true;
+//             buttonCount++;
+//         }
+//     }
+//     else {
+//         newSearch = false;
+//     }
+// }
 
 searchButton.on("click", function(){
-    console.log("hello");
-    if (userCity.value !== ""){
-        fetch(queryURL)
-            .then((response => response.json())
-            .then(userDataProcessing))
-            .then(getCurrentWeatherData)
+    let userCity = $("#enter-city");
+    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.val() + "&appid=" + weatherAPIKey + "&units=imperial";
+    console.log(userCity.val());
+    if (userCity.val() !== ""){
+        // fetch(queryURL)
+        //     .then((response => response.json())
+        //     .then(userDataProcessing))
+        //     .then(getCurrentWeatherData)
+            fetch(queryURL)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        let latitude = data.coord.lat;
+        let longitude = data.coord.lon;
+        console.log(latitude);
+        console.log(data)
+        currentCity.text(data.name);
+        currentTemp.text(data.main.temp);
+        currentWind.text(data.wind.speed);
+        currentHumidity.text(data.main.humidity);
+        fiveDayForecast(latitude, longitude);
+        })
     }
 
 });
+
+function fiveDayForecast(lat, lon){
+    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon=" + lon + "&appid=" + weatherAPIKey + "&units=imperial";
+fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    //   console.log(data.main.temp)
+    })
+}
