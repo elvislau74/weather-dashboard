c0488346655689120b58ff05e5d5cf4585b5b7f6
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-26 17:20:12 -0400

    add function in js to process user data into search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,60 @@
 const weatherAPIKey = "d4ed3cef19cf11d3ba421c419e0afec8";
 let userCity = $("#enter-city");
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=" + weatherAPIKey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=" + weatherAPIKey + "&units=imperial";
 const searchButton = $(".btn");
+let searchHistory = $("#search-history");
+
+let buttonCount = 0;
+let buttonList = [];
+let searchHistoryList = [];
+let newSearch = false;
+
+function getCurrentWeatherData(weather) {
+    if (newSearch === true) {
+        
+    }
+    
+}
+
+function userDataProcessing() {
+    if (userCity.value !== ""){
+        if (searchHistoryList !== null) {
+            if (searchHistoryList.includes(userCity.value) === false){
+                if (buttonCount > 7){
+                    searchHistoryList.shift();
+                    searchHistoryList.push(userCity.value);
+                    $("buttonList[0]").removeAttr("id");
+                    buttonList.shift();
+                }
+                else {
+                    searchHistoryList.push(userCity.value);
+                    buttonCount++;
+                }
+                newSearch = true;
+            }
+            else {
+                newSearch = false;
+            }
+        }
+        else {
+            searchHistoryList = [];
+            searchHistoryList.push(userCity.value);
+            newSearch = true;
+            buttonCount++;
+        }
+    }
+    else {
+        newSearch = false;
+    }
+}
 
 searchButton.on("click", function(){
     console.log("hello");
+    if (userCity.value !== ""){
+        fetch(queryURL)
+            .then((response => response.json())
+            .then(userDataProcessing))
+            .then()
+    }
+
 });
