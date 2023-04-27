30a5f8894d98fc53e3751d2a5191e087e0991bab
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-27 12:01:09 -0400

    add init function and getCurrentWeather function, add id in div in index to better link to localStorage using it as a key

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,44 @@ let currentWind = $(".current-wind");
 let currentHumidity = $(".current-humidity");
 
 function init() {
+  userCity = $("#enter-city");
+  let cityToSearch = localStorage.getItem("search-input");
+  queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + weatherAPIKey + "&units=imperial";
+  $("#search-input").children("#enter-city").val(localStorage.getItem("search-input"));
 
+  if (userCity.val() == "") {
+    cityToSearch = "Philadelphia";
+    userCity.val(cityToSearch);
+    queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + weatherAPIKey + "&units=imperial";
+    getCurrentWeather(queryURL);
+  }
+  else {
+    cityToSearch = localStorage.getItem("search-input");
+    userCity.val(cityToSearch);
+    getCurrentWeather(queryURL);
+  }
+
+};
+
+init();
+
+function getCurrentWeather(queryURL) {
+  fetch(queryURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    latitude = data.coord.lat;
+    longitude = data.coord.lon;
+    console.log(latitude);
+    console.log(data)
+    currentCity.text(data.name);
+    currentTemp.text(data.main.temp);
+    currentWind.text(data.wind.speed);
+    currentHumidity.text(data.main.humidity);
+    fiveDayForecast(latitude, longitude);
+    displayCurrentDate();
+    });
 };
 
 searchButton.on("click", function(){
@@ -17,26 +54,12 @@ searchButton.on("click", function(){
     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity.val() + "&appid=" + weatherAPIKey + "&units=imperial";
     console.log(userCity.val());
     if (userCity.val() !== ""){
-        fetch(queryURL)
-            .then(function (response) {
-            return response.json();
-            })
-            .then(function (data) {
-            let latitude = data.coord.lat;
-            let longitude = data.coord.lon;
-            console.log(latitude);
-            console.log(data)
-            currentCity.text(data.name);
-            currentTemp.text(data.main.temp);
-            currentWind.text(data.wind.speed);
-            currentHumidity.text(data.main.humidity);
-            fiveDayForecast(latitude, longitude);
-            displayCurrentDate();
-            })
+      getCurrentWeather(queryURL);
     }
     let citySearched = userCity.val()
-
-    localStorage.setItem(item, citySearched);
+    let siblingClass = $(this).parent().siblings().attr("id");
+    localStorage.setItem(siblingClass, citySearched);
+    console.log(siblingClass);
 
 });
 
@@ -65,9 +88,9 @@ fetch(queryURL)
         console.log(forecastCount);
       }
     // $("#day0").children().children(".humidity").text(data.list[0].main.humidity);
-    // $("#day2").children().children(".humidity").text(data.list[9].main.humidity);
-    // $("#day3").children().children(".humidity").text(data.list[18].main.humidity);
-    // $("#day4").children().children(".humidity").text(data.list[27].main.humidity);
-    // $("#day5").children().children(".humidity").text(data.list[36].main.humidity);
+    // $("#day1").children().children(".humidity").text(data.list[9].main.humidity);
+    // $("#day2").children().children(".humidity").text(data.list[18].main.humidity);
+    // $("#day3").children().children(".humidity").text(data.list[27].main.humidity);
+    // $("#day4").children().children(".humidity").text(data.list[36].main.humidity);
     })
 }
