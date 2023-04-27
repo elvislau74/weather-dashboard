2986af1d6e87b483d19077b78165b10132722c0c
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-27 17:50:22 -0400

    add comments to the index.html, css, and js, also edit readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ N/A
 
 ## Usage
 
-This website allows users to enter a city name and find it's weather conditions. After typing in a city name and clicking the submit button, current weather conditions in that city, as well as a 5 day forecast of the next few days will be displayed on the screen. Each time the user enters a city, there will be a search history logged below the submit button. If the user clicks on one of the city names in the history, it will redisplay the weather of the city name selected. If the page is refreshed, the city name entered by the user will persist on the page.  
+This website allows users to enter a city name and find it's weather conditions. When loading in the page for the first time, the page will have a default city of Philadelphia. After typing in a city name and clicking the submit button, current weather conditions in that city, as well as a 5 day forecast of the next few days will be displayed on the screen. Each time the user enters a city, there will be a search history logged below the submit button. If the user clicks on one of the city names in the history, it will redisplay the weather of the city name selected. If the page is refreshed, the city name entered by the user will persist on the page.  
 
 <img src="#" width = 500px />
 
@@ -22,7 +22,11 @@ This website allows users to enter a city name and find it's weather conditions.
 
 ## Credits
 
-N/A
+Special thanks to my tutor as well as my fellow bootcamp student/colleague who helped to guide me to the right direction when I was lost during this challenge.
+
+<a href="https://github.com/katredford">Katie Redford</a>
+
+<a href="https://github.com/myrojoylee">Myro Joy Lee</a>
 
 ## License 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// API key along with all the variables
 const weatherAPIKey = "d4ed3cef19cf11d3ba421c419e0afec8";
 let forecastCount;
 let weatherIcon;
@@ -11,6 +12,8 @@ let currentWind = $(".current-wind");
 let currentHumidity = $(".current-humidity");
 let monthNow, dayNow, yearNow, dateNow, unixDate, dateConverted;
 
+// Unix time converter function to convert dates pulled from the API
+// the time conversion methods were taught to me from a fellow bootcamp student/colleague, Myro Joy Lee
 function convertUnixTime(unixDate) {
   dateConverted = new Date(unixDate*1000);
   monthNow = dateConverted.getMonth() + 1;
@@ -20,6 +23,9 @@ function convertUnixTime(unixDate) {
   return dateNow;
 };
 
+// this init function causes the page to be set to a default city, "Philadelphia"
+// and pulls from local storage to change that default after user input
+// on refresh, the city weather information inputted by the guest will persist on the page
 function init() {
   userCity = $("#enter-city");
   let cityToSearch = localStorage.getItem("search-input");
@@ -37,11 +43,13 @@ function init() {
     userCity.val(cityToSearch);
     getCurrentWeather(queryURL);
   }
-
 };
 
+// Calling the init function
 init();
 
+// This function fetches the current weather data from the API, linking some to variables for further use
+// and also pastes some of that information onto the html page.
 function getCurrentWeather(queryURL) {
   fetch(queryURL)
     .then(function (response) {
@@ -52,24 +60,23 @@ function getCurrentWeather(queryURL) {
     longitude = data.coord.lon;
     weatherIcon = data.weather[0].icon;
     unixDate = data.dt;
-    console.log(unixDate);
-    console.log(latitude);
-    console.log(data);
-    console.log(weatherIcon);
     currentCity.text(data.name);
     $(".city-and-date").children(".icon").attr("src", `https://openweathermap.org/img/wn/${weatherIcon}.png`);
     currentDate.text(convertUnixTime(unixDate));
     currentTemp.text(data.main.temp);
     currentWind.text(data.wind.speed);
     currentHumidity.text(data.main.humidity);
+    // Calls the function to fetch the five day forecast with inputs of longitude and latitude
     fiveDayForecast(latitude, longitude);
     });
 };
 
+// Click event listener
+// It calls on the getCurrentWeather function to pull data from the API and put on page
+// It also grabs user input from the page, setting it to local storage for later use
 searchButton.on("click", function(){
     let userCity = $("#enter-city");
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.val() + "&appid=" + weatherAPIKey + "&units=imperial";
-    console.log(userCity.val());
     cityToSearch = userCity.value;
     if (userCity.val() !== ""){
       getCurrentWeather(queryURL);
@@ -77,10 +84,8 @@ searchButton.on("click", function(){
     let citySearched = userCity.val()
     let siblingClass = $(this).parent().siblings().attr("id");
     localStorage.setItem(siblingClass, citySearched);
-    console.log(siblingClass);
 
     // createHistoryButtons(cityToSearch);
-
 });
 
 // function createHistoryButtons(cityToSearch) {
@@ -92,6 +97,7 @@ searchButton.on("click", function(){
 //   // historyButton.attr("")
 // }
 
+// this function fetches information from the API to use for the five day forecast portion of the page
 function fiveDayForecast(lat, lon){
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon=" + lon + "&appid=" + weatherAPIKey + "&units=imperial";
 fetch(queryURL)
@@ -99,21 +105,17 @@ fetch(queryURL)
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data.list[9].main.humidity);
       forecastCount = 0;
       for (var i = 0; i < 5; i++) {
         let fivedays = $(`#day${i}`);
         weatherIcon = data.list[forecastCount].weather[0].icon;
         unixDate = data.list[forecastCount].dt;
         fivedays.children(".date").text(convertUnixTime(unixDate));
-        console.log(data.list[forecastCount].dt_txt);
         fivedays.children().attr("src", `https://openweathermap.org/img/wn/${weatherIcon}.png`)
         fivedays.children().children(".temp").text(data.list[forecastCount].main.temp);
         fivedays.children().children(".wind").text(data.list[forecastCount].wind.speed);
         fivedays.children().children(".humidity").text(data.list[forecastCount].main.humidity);
         forecastCount = forecastCount + 8;
-        console.log(forecastCount);
       }
     })
 }
