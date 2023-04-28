1a9b23b972d58bc7e1386654a43f3038d4911531
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-27 20:46:12 -0400

    add function to create history buttons, fix timezone issue in 5 day forecast, redo code for init function, make event listener for newly created history buttons, fixed localstorage and made it take in an array of values, add screenshot of live page into images and link it to readme

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ N/A
 
 This website allows users to enter a city name and find it's weather conditions. When loading in the page for the first time, the page will have a default city of Philadelphia. After typing in a city name and clicking the submit button, current weather conditions in that city, as well as a 5 day forecast of the next few days will be displayed on the screen. Each time the user enters a city, there will be a search history logged below the submit button. If the user clicks on one of the city names in the history, it will redisplay the weather of the city name selected. If the page is refreshed, the city name entered by the user will persist on the page.  
 
-<img src="#" width = 500px />
+<img src="./assets/images/weather-dashboard-livesite.png" width = 500px />
 
 <a href="https://github.com/elvislau74/weather-dashboard">Click here for the GitHub Repository</a>
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const weatherAPIKey = "d4ed3cef19cf11d3ba421c419e0afec8";
 let forecastCount;
 let weatherIcon;
+let cityHistory = [];
 let longitude, latitude;
 const searchButton = $(".btn");
 let searchHistory = $("#search-history");
@@ -10,12 +11,14 @@ let currentDate = $(".current-date");
 let currentTemp = $(".current-temp");
 let currentWind = $(".current-wind");
 let currentHumidity = $(".current-humidity");
-let monthNow, dayNow, yearNow, dateNow, unixDate, dateConverted;
+let monthNow, dayNow, yearNow, dateNow, unixDate, dateConverted, currentTime;
 
 // Unix time converter function to convert dates pulled from the API
 // the time conversion methods were taught to me from a fellow bootcamp student/colleague, Myro Joy Lee
+// the getTimezoneOffset method was code my instructor, Anthony Maddatu gave me
 function convertUnixTime(unixDate) {
-  dateConverted = new Date(unixDate*1000);
+  currentTime = new Date();
+  dateConverted = new Date(unixDate*1000 + currentTime.getTimezoneOffset() * 60 * 1000);
   monthNow = dateConverted.getMonth() + 1;
   dayNow = dateConverted.getDate();
   yearNow = dateConverted.getFullYear();
@@ -23,24 +26,23 @@ function convertUnixTime(unixDate) {
   return dateNow;
 };
 
+
 // this init function causes the page to be set to a default city, "Philadelphia"
 // and pulls from local storage to change that default after user input
 // on refresh, the city weather information inputted by the guest will persist on the page
 function init() {
-  userCity = $("#enter-city");
-  let cityToSearch = localStorage.getItem("search-input");
-  queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + weatherAPIKey + "&units=imperial";
-  $("#search-input").children("#enter-city").val(localStorage.getItem("search-input"));
-
-  if (userCity.val() == "") {
-    cityToSearch = "Philadelphia";
-    userCity.val(cityToSearch);
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + weatherAPIKey + "&units=imperial";
-    getCurrentWeather(queryURL);
+  var histTemp = localStorage.getItem("cityHistory");
+  if(histTemp){ // if exists
+    cityHistory = JSON.parse(histTemp);
+  }
+  createHistoryButtons();
+  var enterTemp = localStorage.getItem("entered");
+  if(enterTemp){ // if exists
+    getCurrentWeather(enterTemp);
   }
   else {
-    cityToSearch = localStorage.getItem("search-input");
-    userCity.val(cityToSearch);
+    cityToSearch = "Philadelphia"
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + weatherAPIKey + "&units=imperial";
     getCurrentWeather(queryURL);
   }
 };
@@ -51,6 +53,7 @@ init();
 // This function fetches the current weather data from the API, linking some to variables for further use
 // and also pastes some of that information onto the html page.
 function getCurrentWeather(queryURL) {
+  localStorage.setItem("entered", queryURL);
   fetch(queryURL)
     .then(function (response) {
     return response.json();
@@ -73,29 +76,39 @@ function getCurrentWeather(queryURL) {
 
 // Click event listener
 // It calls on the getCurrentWeather function to pull data from the API and put on page
-// It also grabs user input from the page, setting it to local storage for later use
+// It also grabs user input from the page, setting it to an array in local storage for later use
 searchButton.on("click", function(){
     let userCity = $("#enter-city");
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.val() + "&appid=" + weatherAPIKey + "&units=imperial";
-    cityToSearch = userCity.value;
+    cityToSearch = userCity.val();
     if (userCity.val() !== ""){
       getCurrentWeather(queryURL);
     }
-    let citySearched = userCity.val()
-    let siblingClass = $(this).parent().siblings().attr("id");
-    localStorage.setItem(siblingClass, citySearched);
+    cityHistory.push(cityToSearch);
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 
-    // createHistoryButtons(cityToSearch);
+    createHistoryButtons();
 });
 
-// function createHistoryButtons(cityToSearch) {
-//   let historyButton = $("<button>");
-//   historyButton.text(cityToSearch);
-//   historyButton.attr("class", "history-btn");
-//   searchHistory.append(historyButton);
-//   searchHistory.css("display", "flex")
-//   // historyButton.attr("")
-// }
+// Create history buttons with this function
+function createHistoryButtons() {
+  searchHistory.empty();
+  for (let i = 0; i < cityHistory.length; i++){
+  let historyButton = $("<button>");
+  historyButton.text(cityHistory[i]);
+  historyButton.val(cityHistory[i]);
+  historyButton.attr("class", "history-btn");
+  searchHistory.append(historyButton);
+  searchHistory.css("display", "flex");
+  searchHistory.css("flex-wrap", "wrap");
+  }
+}
+
+// Create event listener for the search history buttons that will also update the page when clicked
+searchHistory.on("click", ".history-btn", function(){
+  queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $(this).val() + "&appid=" + weatherAPIKey + "&units=imperial";
+  getCurrentWeather(queryURL);
+})
 
 // this function fetches information from the API to use for the five day forecast portion of the page
 function fiveDayForecast(lat, lon){
@@ -106,10 +119,13 @@ fetch(queryURL)
     })
     .then(function (data) {
       forecastCount = 0;
-      for (var i = 0; i < 5; i++) {
+      console.log(data);
+      for (let i = 0; i < 5; i++) {
         let fivedays = $(`#day${i}`);
+        console.log(data.list[forecastCount]);
         weatherIcon = data.list[forecastCount].weather[0].icon;
         unixDate = data.list[forecastCount].dt;
+        console.log(unixDate);
         fivedays.children(".date").text(convertUnixTime(unixDate));
         fivedays.children().attr("src", `https://openweathermap.org/img/wn/${weatherIcon}.png`)
         fivedays.children().children(".temp").text(data.list[forecastCount].main.temp);
